b5ddfa6441eb088083af3b92646988eb483c1a61
Axagon79 <lollotj@yahoo.it>
2024-12-15 15:45:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,14 +7,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const MONGODB_URI = process.env.MONGODB_URI; // Rimuovi la parte "|| 'mongodb://127.0.0.1:27017/pup-pals'"
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Aggiungi il console.log qui:
 console.log('MONGODB_URI:', MONGODB_URI);
 
 mongoose.connect(MONGODB_URI)
@@ -23,13 +22,11 @@ mongoose.connect(MONGODB_URI)
 
 // Importa le rotte
 const userRoutes = require('./routes/userRoutes');
-const dogRoutes = require('./routes/dogRoutes'); // Nuova linea
+const dogRoutes = require('./routes/dogRoutes');
 
-console.log('Tipo di userRoutes:', typeof userRoutes);
-console.log('Chiavi di userRoutes:', Object.keys(userRoutes));
-
-app.use('/users', userRoutes);
-app.use('/api/dogs', dogRoutes); // Nuova linea
+// Aggiungi il prefisso /api a userRoutes
+app.use('/api/users', userRoutes);
+app.use('/api/dogs', dogRoutes);
 
 // Avvio del server
 app.listen(port, () => {
