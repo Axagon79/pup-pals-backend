410da434b071386628b8dcec4302c036428ebf80
Axagon79 <lollotj@yahoo.it>
2024-12-25 02:55:17 +0100

    Update Firebase config to use secret file

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,6 +1,7 @@
 const path = require('path');
-const serviceAccount = require(path.resolve('./config/firebase-adminsdk.json'));
+const serviceAccount = require('/etc/secrets/firebase-adminsdk.json');
 
+const admin = require('firebase-admin');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
