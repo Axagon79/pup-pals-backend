1aedeee1c52bff43ae759e8ade25c581fed455a7
Axagon79 <lollotj@yahoo.it>
2024-12-25 03:52:59 +0100

    Aggiornata configurazione Firebase Admin

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,9 +1,14 @@
 const path = require('path');
-const serviceAccount = require('/etc/secrets/firebase-adminsdk.json');
+const serviceAccount = require('./config/firebase-adminsdk.json');
 
 const admin = require('firebase-admin');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+} catch (error) {
+  console.error('Errore inizializzazione Firebase Admin:', error);
+}
 
 module.exports = admin;
