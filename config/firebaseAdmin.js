067d420c0a4e9d16dd8ca7ae30c57f0eea864bba
Axagon79 <lollotj@yahoo.it>
2024-12-25 01:08:25 +0100

    Aggiornato percorso file Firebase Admin

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./puppals-456c7-firebase-adminsdk-jan27-bd6e6528c5.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
