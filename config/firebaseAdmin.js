635406836d7b5f56bc6ca09e556f828475b9e962
Axagon79 <lollotj@yahoo.it>
2024-12-28 04:31:22 +0100

    test firebase

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,6 +1,11 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('/etc/secrets/firebase-adminsdk.json'); // Percorso per i Secret Files su Render
+// Aggiungi queste due righe all'inizio del file:
+const test = require('/etc/secrets/test.txt');
+console.log("Contenuto di test.txt:", test);
+
+// Il resto del tuo codice per l'inizializzazione di Firebase Admin SDK:
+const serviceAccount = require('/etc/secrets/firebase-adminsdk.json');
 
 try {
   admin.initializeApp({
